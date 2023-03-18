7ae6a5dfc9711e933feab1f6cd4ab790e4cee819
Pierre Martinon <pierrecmartinon@gmail.com>
2023-03-18 12:36:02 +0100

    debug build.bat

--- bocop/examples/vanderpol/build.bat
+++ bocop/examples/vanderpol/build.bat
@@ -1,11 +1,18 @@
 set problem_dir=%~dp0
 set bocop_root_path=..\..\..
 
+dir %bocop_root_path%
+dir
+
 rmdir /s /q build
 mkdir build
 cd build
+echo "Go to build subdir"
+dir
 
+echo "cmake step"
 cmake -G "NMake Makefiles" -DPROBLEM_DIR=%problem_dir% %bocop_root_path%
+echo "nmake step"
 nmake
 
 cd.. 
