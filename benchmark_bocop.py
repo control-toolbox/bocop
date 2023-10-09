389febe5e663b54260df0897a9c34a7a9c6817f8
Pierre Martinon <pierrecmartinon@gmail.com>
2023-10-09 16:34:16 +0200

    some benchmark logs

--- benchmark_bocop.py
+++ benchmark_bocop.py
@@ -3,9 +3,12 @@ import sys
 import bocop
 
 bocop.hello("Launching bocop benchmark...")
-opt_dict = {"time.steps": "1000", 
+opt_dict = {"ipoptNumOption.tol": "1e-8",
+            "ipoptIntOption.max_iter": "1000",
+            "ipoptStrOption.mu_strategy": "adaptive",
             "ode.discretization": "midpoint_implicit",
-            "ipoptIntOption.max_iter": "500",
-            "ipoptStrOption.mu_strategy": "adaptive"}
+            #"ode.discretization": "gauss3",
+            "time.steps": "1000",
+}
 ret = bocop.test(examples_list_prefix='./bocop/test/benchmark', benchmark=1, broadcast_options=opt_dict)
 exit(ret)
